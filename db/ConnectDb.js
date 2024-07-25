e2d100a359f72992abf9a350ce6c2f78156c9895
vishakha12345678 <vishakhasaxena1998@gmail.com>
2024-07-25 22:19:46 +0530

    Update ConnectDb.js

--- db/ConnectDb.js
+++ db/ConnectDb.js
@@ -3,7 +3,7 @@ const local_url = 'mongodb://127.0.0.1:27017/Admissionproject';
 const live_url = 'mongodb+srv://vishakha:vishakha1998@cluster0.9u3vwbx.mongodb.net/admission-portal?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectDb = () => {
-    return mongoose.connect(local_url)
+    return mongoose.connect(live_url)
           .then(()=>{
             console.log('connect successfully');
           }).catch((error)=>{
